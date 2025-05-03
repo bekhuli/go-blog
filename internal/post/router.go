a9f7dd81a8a00288064c4679970e4aa7cfec4af5
bekhuli <bbbbek37@gmail.com>
2025-05-03 21:55:28 +0500

    post router

---
+++ internal/post/router.go
@@ -0,0 +1,22 @@
+package post
+
+import (
+	"github.com/bekhuli/go-blog/internal/common"
+	"github.com/bekhuli/go-blog/pkg/auth"
+
+	"github.com/gorilla/mux"
+)
+
+func RegisterPostRoutes(r *mux.Router, h *PostHandler) {
+	// PUBLIC
+	r.HandleFunc("/posts", h.ListPosts).Methods("GET")
+	r.HandleFunc("/posts/{id}", h.GetPostByID).Methods("GET")
+
+	// SECURED
+	protected := r.NewRoute().Subrouter()
+	protected.Use(auth.JWTMiddleware(common.JWTEnv))
+
+	protected.HandleFunc("/posts/{id}", h.GetPostByID).Methods("GET")
+	protected.HandleFunc("/posts/{id}", h.UpdatePost).Methods("PUT")
+	protected.HandleFunc("/posts/{id}", h.DeletePost).Methods("DELETE")
+}
--- internal/routes/router.go
+++ internal/routes/router.go
@@ -2,7 +2,10 @@ package routes
 
 import (
 	"database/sql"
+
+	"github.com/bekhuli/go-blog/internal/post"
 	"github.com/bekhuli/go-blog/internal/user"
+
 	"github.com/gorilla/mux"
 )
 
@@ -11,12 +14,21 @@ func InitRouter(db *sql.DB) *mux.Router {
 
 	api := r.PathPrefix("/api/v1").Subrouter()
 
-	userRepo := user.NewSQLRepository(db)
-	userValidator := user.NewValidator()
-	userService := user.NewService(userRepo, userValidator)
+	// --- USER ---
+	userRepo := user.NewUserRepository(db)
+	userValidator := user.NewUserValidator()
+	userService := user.NewUserService(userRepo, userValidator)
 	userHandler := user.NewUserHandler(userService)
 
-	user.RegisterRoutes(api, userHandler)
+	user.RegisterUserRoutes(api, userHandler)
+
+	// --- POST ---
+	postRepo := post.NewPostRepository(db)
+	postValidator := post.NewPostValidator()
+	postService := post.NewPostService(postRepo, postValidator)
+	postHandler := post.NewPostHandler(postService)
+
+	post.RegisterPostRoutes(api, postHandler)
 
 	return r
 }
