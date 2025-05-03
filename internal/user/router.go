89649f55d29eee68d667e8d77694509840abee88
bekhuli <bbbbek37@gmail.com>
2025-05-03 21:27:09 +0500

    router namechange

--- internal/user/router.go
+++ internal/user/router.go
@@ -2,7 +2,7 @@ package user
 
 import "github.com/gorilla/mux"
 
-func RegisterRoutes(r *mux.Router, h *UserHandler) {
+func RegisterUserRoutes(r *mux.Router, h *UserHandler) {
 	r.HandleFunc("/register", h.RegisterUser).Methods("POST")
 	r.HandleFunc("/login", h.LoginUser).Methods("POST")
 }
